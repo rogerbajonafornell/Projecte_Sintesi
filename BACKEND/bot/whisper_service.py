7080718f700453174dbadecdc84595d2a62745a2
rogerbajona <roger.bajona@escolaarrels.com>
2025-03-27 19:21:32 +0100

    Descripció del canvi

--- BACKEND/bot/whisper_service.py
+++ BACKEND/bot/whisper_service.py
@@ -1,7 +1,7 @@
 # whisper_service.py
 from faster_whisper import WhisperModel
 
-# Carrega el model; pots ajustar "medium" per un model més petit o més gran segons els teus recursos
+# Carrega el model en aquest cas el medium que és el que s'adapta millor al nostre projecte.
 model = WhisperModel("medium", device="cpu", compute_type="int8")
 
 def transcribe_audio(file_path):
@@ -9,6 +9,6 @@ def transcribe_audio(file_path):
     Transcriu el fitxer d'àudio indicat i detecta automàticament l'idioma.
     Retorna una tupla (transcripció, idioma).
     """
-    segments, info = model.transcribe(file_path, beam_size=5)
+    segments, info = model.transcribe(file_path, beam_size=5) # beam_sixe: Equilibri velocitat/precisió
     transcription = " ".join(segment.text for segment in segments)
     return transcription, info.language
