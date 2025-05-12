347e9ac48199aa071e8d31d439acb680a2e793fe
rogerbajona <roger.bajona@escolaarrels.com>
2025-05-12 20:00:31 +0200

    fixes

--- BACKEND/bot/utils.py
+++ BACKEND/bot/utils.py
@@ -1,24 +1,24 @@
-import openai
-import pinecone
-import os
+#import openai
+#import pinecone
+#import os
 # Configuració d'OpenAI
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Configuració de Pinecone
-pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV"))
-INDEX_NAME = "articles"  # Nom de l'índex a Pinecone, assegura't que existeixi
+# # Configuració de Pinecone
+# pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV"))
+# INDEX_NAME = "articles"  # Nom de l'índex a Pinecone, assegura't que existeixi
 
-def generate_embedding(text):
-    """Genera un embedding per a un text donat utilitzant OpenAI."""
-    response = openai.Embedding.create(
-        input=text,
-        model="text-embedding-3-small"
-    )
-    return response['data'][0]['embedding']
+# def generate_embedding(text):
+#     """Genera un embedding per a un text donat utilitzant OpenAI."""
+#     response = openai.Embedding.create(
+#         input=text,
+#         model="text-embedding-3-small"
+#     )
+#     return response['data'][0]['embedding']
 
-def search_similar_articles(query, top_k=5):
-    """Cerca articles similars a Pinecone utilitzant l'embedding de la consulta."""
-    query_embedding = generate_embedding(query)
-    index = pinecone.Index(INDEX_NAME)
-    results = index.query(vector=query_embedding, top_k=top_k, include_values=False)
-    return [int(match.id) for match in results.matches]
+# def search_similar_articles(query, top_k=5):
+#     """Cerca articles similars a Pinecone utilitzant l'embedding de la consulta."""
+#     query_embedding = generate_embedding(query)
+#     index = pinecone.Index(INDEX_NAME)
+#     results = index.query(vector=query_embedding, top_k=top_k, include_values=False)
+#     return [int(match.id) for match in results.matches]
--- BACKEND/bot/views.py
+++ BACKEND/bot/views.py
@@ -9,11 +9,13 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponseBadRequest
 from dotenv import load_dotenv
 from decimal import Decimal
-from .utils import search_similar_articles
+#from .utils import search_similar_articles
 from inventari.models import Article
 from bot.models import Comanda, Usuari
 from .serializers import UsuariSerializer, ComandaSerializer
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
  
 # Carregar variables d'entorn
 load_dotenv()
@@ -285,14 +287,14 @@ def buscar_article(descripcio):
     except Article.DoesNotExist:
         print(f"❌ Article.DoesNotExist for key: {key}")
         ### UN COP L'ARTICLE NO ES TROBA DE MANERA LITERAL EM BUSQUEM AMB ENDPOINTS GRÀCIES A PINECONE
-        similar_ids = search_similar_articles(key)
+        """  similar_ids = search_similar_articles(key)
         if similar_ids:
             similar_articles = Article.objects.filter(CodigoArticulo__in=similar_ids)
             print(f"🔍 Found similar articles: {[a.DescripcionArticulo for a in similar_articles]}")
             return similar_articles  # Retorna un QuerySet amb articles similars
         else:
             print(f"❌ No similar articles found")
-            return None
+            return None """
 
 
 def actualitzar_unidades(article, quantitat):
@@ -330,7 +332,7 @@ class ComandaDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
-        article = instance.Article
+        article = instance.article
 
         # Sumar la cantidad de vuelta al inventario
         article.Unidades += instance.Quantitat
